11cb31d1c42452385164360bc8f35549a86bb374
220350 <220350@E0403-04>
2023-05-16 15:32:35 +0900

    プロジェクト ファイルを追加します。

---
+++ Main.cpp
@@ -0,0 +1,75 @@
+#include <Windows.h>
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
+{
+  //create window class
+    WNDCLASSEX wc;
+    wc.cbSize = sizeof(WNDCLASSEX);             //この構造体のサイズ
+    wc.hInstance = hInstance;                   //インスタンスハンドル
+    wc.lpszClassName = "SampleGame";            //ウィンドウクラス名
+    wc.lpfnWndProc = WndProc;                   //ウィンドウプロシージャ
+    wc.style = CS_VREDRAW | CS_HREDRAW;         //スタイル（デフォルト）
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION); //アイコン
+    wc.hIconSm = LoadIcon(NULL, IDI_WINLOGO);   //小さいアイコン
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);   //マウスカーソル
+    wc.lpszMenuName = NULL;                     //メニュー（なし）
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH); //背景（白）
+
+    RegisterClassEx(&wc);
+
+  //create window
+    HWND hWnd = CreateWindow(
+        "SampleGame",         //ウィンドウクラス名
+        "サンプルゲーム",     //タイトルバーに表示する内容
+        WS_OVERLAPPEDWINDOW, //スタイル（普通のウィンドウ）
+        CW_USEDEFAULT,       //表示位置左（おまかせ）
+        CW_USEDEFAULT,       //表示位置上（おまかせ）
+        800,                 //ウィンドウ幅
+        600,                 //ウィンドウ高さ
+        NULL,                //親ウインドウ（なし）
+        NULL,                //メニュー（なし）
+        hInstance,           //インスタンス
+        NULL                 //パラメータ（なし）
+    );
+
+  //output window
+    ShowWindow(hWnd, nCmdShow);
+
+  //message loop (waiting for order some )
+    MSG msg;
+    ZeroMemory(&msg, sizeof(msg));
+    while (msg.message != WM_QUIT)
+    {
+        //メッセージあり
+        if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        //メッセージなし
+        else
+        {
+            //ゲームの処理
+
+
+        }
+    }
+
+	return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);  //プログラム終了
+        return 0;
+    }
+    return DefWindowProc(hWnd, msg, wParam, lParam);
+}
