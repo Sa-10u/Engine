0cd370167b5fa7cf1ec19fd9ec8b9dc02eee604c
220350 <220350@E0404-04>
2023-05-23 11:27:02 +0900

    the grapgics

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,15 @@
 #include <Windows.h>
+#include<d3d11.h>
+
+#pragma comment(lib, "d3d11.lib")
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+void Edit_d3d(HWND hWnd);
+
+ID3D11Device* pDevice;		//デバイス
+ID3D11DeviceContext* pContext;		//デバイスコンテキスト
+IDXGISwapChain* pSwapChain;		//スワップチェイン
+ID3D11RenderTargetView* pRenderTargetView;
 
 namespace WIN
 {
@@ -25,8 +34,8 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, 
     wc.lpszClassName = WIN::_NAME;            //ウィンドウクラス名
     wc.lpfnWndProc = WndProc;                   //ウィンドウプロシージャ
     wc.style = CS_VREDRAW | CS_HREDRAW;         //スタイル（デフォルト）
-    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION); //アイコン
-    wc.hIconSm = LoadIcon(NULL, IDI_WINLOGO);   //小さいアイコン
+    wc.hIcon = LoadIcon(NULL, IDI_QUESTION); //アイコン
+    wc.hIconSm = LoadIcon(NULL, IDI_QUESTION);   //小さいアイコン
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);   //マウスカーソル
     wc.lpszMenuName = NULL;                     //メニュー（なし）
     wc.cbClsExtra = 0;
@@ -53,6 +62,10 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, 
   //output window
     ShowWindow(hWnd, nCmdShow);
 
+    //-----------
+    Edit_d3d(hWnd);
+
+
   //message loop (waiting for order some )
     MSG msg;
     ZeroMemory(&msg, sizeof(msg));
@@ -70,9 +83,25 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, 
         {
             //ゲームの処理
 
+            float clearColor[4] = { 0.0f, 0.5f, 0.5f, 1.0f };//R,G,B,A
+
+            //画面をクリア
+            pContext->ClearRenderTargetView(pRenderTargetView, clearColor);
+
+
+            //描画処理
+
+
+            //スワップ（バックバッファを表に表示する）
+            pSwapChain->Present(0, 0);
 
         }
     }
+
+    pRenderTargetView->Release();
+    pSwapChain->Release();
+    pContext->Release();
+    pDevice->Release();
 
 	return 0;
 }
@@ -86,4 +115,69 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
         return 0;
     }
     return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+void Edit_d3d(HWND hWnd)
+{
+    DXGI_SWAP_CHAIN_DESC scDesc;
+
+    //とりあえず全部0
+    ZeroMemory(&scDesc, sizeof(scDesc));
+
+    //描画先のフォーマット
+    scDesc.BufferDesc.Width = WIN::_WIDTH;		//画面幅
+    scDesc.BufferDesc.Height = WIN::_HEIGHT;	//画面高さ
+    scDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;	// 何色使えるか
+
+    //FPS（1/60秒に1回）
+    scDesc.BufferDesc.RefreshRate.Numerator = 60;
+    scDesc.BufferDesc.RefreshRate.Denominator = 1;
+
+    //その他
+    scDesc.Windowed = TRUE;			//ウィンドウモードかフルスクリーンか
+    scDesc.OutputWindow = hWnd;		//ウィンドウハンドル
+    scDesc.BufferCount = 1;			//バックバッファの枚数
+    scDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;	//バックバッファの使い道＝画面に描画するために
+    scDesc.SampleDesc.Count = 1;		//MSAA（アンチエイリアス）の設定
+    scDesc.SampleDesc.Quality = 0;		//　〃
+
+    //------
+    D3D_FEATURE_LEVEL level;
+    D3D11CreateDeviceAndSwapChain(
+        nullptr,				// どのビデオアダプタを使用するか？既定ならばnullptrで
+        D3D_DRIVER_TYPE_HARDWARE,		// ドライバのタイプを渡す。ふつうはHARDWARE
+        nullptr,				// 上記をD3D_DRIVER_TYPE_SOFTWAREに設定しないかぎりnullptr
+        0,					// 何らかのフラグを指定する。（デバッグ時はD3D11_CREATE_DEVICE_DEBUG？）
+        nullptr,				// デバイス、コンテキストのレベルを設定。nullptrにしとけばOK
+        0,					// 上の引数でレベルを何個指定したか
+        D3D11_SDK_VERSION,			// SDKのバージョン。必ずこの値
+        &scDesc,				// 上でいろいろ設定した構造体
+        &pSwapChain,				// 無事完成したSwapChainのアドレスが返ってくる
+        &pDevice,				// 無事完成したDeviceアドレスが返ってくる
+        &level,					// 無事完成したDevice、Contextのレベルが返ってくる
+        &pContext);
+
+    //-----
+    ID3D11Texture2D* pBackBuffer;
+    pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+
+    //レンダーターゲットビューを作成
+    pDevice->CreateRenderTargetView(pBackBuffer, NULL, &pRenderTargetView);
+
+    //一時的にバックバッファを取得しただけなので解放
+    pBackBuffer->Release();
+
+    //-----
+    D3D11_VIEWPORT vp;
+    vp.Width = (float)WIN::_WIDTH;	//幅
+    vp.Height = (float)WIN::_HEIGHT;//高さ
+    vp.MinDepth = 0.0f;	//手前
+    vp.MaxDepth = 1.0f;	//奥
+    vp.TopLeftX = 0;	//左
+    vp.TopLeftY = 0;	//上
+
+    //データを画面に描画するための一通りの設定（パイプライン）
+    pContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);  // データの入力種類を指定
+    pContext->OMSetRenderTargets(1, &pRenderTargetView, nullptr);            // 描画先を設定
+    pContext->RSSetViewports(1, &vp);
 }
